345712926437c638b13a1a34cbc36a1f1c8e9f36
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-03 20:52:20 -0300

    Primeira aula de csharp

---
+++ Csharp/01.Fiap.HelloWorld.Console.UI/01.Fiap.HelloWorld.Console.UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using _01.Fiap.HelloWorld.Console.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Fiap.HelloWorld.Console.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar um carro
+
+            Carro carro = new Carro();
+            carro.Ano = 2010; //Set
+            carro.ArCondicionado = false; //Set
+
+            System.Console.WriteLine(carro.Ano); //Ano
+
+            //Instanciar uma lancha
+            var lancha = new Lancha()
+            {
+                BoiaSalvaVidas = true,
+                Ano = 1999
+            };
+        }
+    }
+}
