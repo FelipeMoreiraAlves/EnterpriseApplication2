3843e3fa89b2017d6e2984e440fe231024a1856f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-10 20:49:27 -0300

    Correção de exercicios e inicio de ASP.NET

--- 01.Fiap.HelloWorld.Console.UI/Exercicio-01/Program.cs
+++ 01.Fiap.HelloWorld.Console.UI/Exercicio-01/Program.cs
@@ -10,7 +10,35 @@ namespace Exercicio_01
     {
         static void Main(string[] args)
         {
+            //Instanciar as classes ContaCorrente e ContaPoupança
 
+            var cc = new ContaCorrente()
+            {
+                Agencia = 1,
+                Numero = 2,
+                Saldo = 10,
+                Tipo = Model.TipoConta.Comum,
+                DataAbertura = DateTime.Now
+            };
+            var cp = new ContaPoupanca(0.06m)
+            {
+                Agencia = 1,
+                Numero = 2,
+                Saldo = 1000,
+                Taxa = 2,
+                DataAbertura = new DateTime(2018, 1, 1)
+            };
+            try
+            {
+                cc.Retirar(100);
+                Console.WriteLine(cc.Saldo);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
