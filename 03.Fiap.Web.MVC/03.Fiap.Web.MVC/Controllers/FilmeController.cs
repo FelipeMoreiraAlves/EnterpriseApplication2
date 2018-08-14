2a17eff10eae615fe37f9e546878afdafddcfe76
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-13 22:54:29 -0300

    Asp.NET MVC

---
+++ 03.Fiap.Web.MVC/03.Fiap.Web.MVC/Controllers/FilmeController.cs
@@ -0,0 +1,30 @@
+﻿using _03.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03.Fiap.Web.MVC.Controllers
+{
+    public class FilmeController : Controller
+    {
+        // GET: Filme
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Filme filme)
+        {
+            //Passar valores para a view (pagina html)
+            ViewBag.titulo = filme.Nome;
+            TempData["msg"] = "Filme cadastrado!!";
+            return View(filme); //envia o filme para a tela
+            //retorna um texto para o browser
+            //return Content(filme.Genero);
+        }
+    }
+}
