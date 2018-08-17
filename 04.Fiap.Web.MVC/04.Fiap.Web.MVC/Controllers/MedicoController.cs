deb1d4a6756fd9c933a5297086c612ff70109518
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-17 20:44:28 -0300

    Html Helpers e Razors, adicionando redirect para evitar conflitos de reenvio de formulario

---
+++ 04.Fiap.Web.MVC/04.Fiap.Web.MVC/Controllers/MedicoController.cs
@@ -0,0 +1,37 @@
+﻿using _04.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04.Fiap.Web.MVC.Controllers
+{
+    public class MedicoController : Controller
+    {
+        //Simular um banco de dados
+        private static List<Medico> _banco = new List<Medico>();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Medico medico)
+        {
+            _banco.Add(medico); //adiciona no "Banco"
+            TempData["msg"] = "Médico cadastrado!"; //mensagem enviada para a tela
+            //faz redirect para não cadastrar vários no mesmo formulário após o F5
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            //Envia a lista de médicos para a tela
+            return View(_banco);
+        }
+    }
+}
