e4043e198f43f9318a9c1edbde226cd97d7b0699
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-03 22:51:43 -0300

    Relacionamentos (FALTA CADASTRAR JOGADOR)

---
+++ Fiap07.Web.MVC/Fiap07.Web.MVC/Controllers/TimesController.cs
@@ -0,0 +1,32 @@
+﻿using Fiap07.Web.MVC.Models;
+using Fiap07.Web.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap07.Web.MVC.Controllers
+{
+    public class TimesController : Controller
+    {
+        private BrasfootContext _context = new BrasfootContext();
+        // GET: Times
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Time time)
+        {
+            var context = new BrasfootContext();
+            context.Times.Add(time);
+            context.SaveChanges();
+
+            TempData["msg"] = "Cadastrado com sucesso!";
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
